1b142a169c9bd87a39e319986639eaad0205ce32
Lianne Dakanno <liannedakanno@gmail.com>
2022-07-07 02:00:15 -0500

    commit tree 095a847b7a11d58e102c3b9d007f26acd943cea0

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let cardMessages = [];
+    for (let i = 0; i < names.length; i++) {
+        cardMessages[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    }
+    return cardMessages;
+}
+
+function countDown(number) {
+    let i = 0;
+    while (i <= 10) {
+        console.log(i);
+        i++;
+    }
+}
